40aec8bac31b7b6da169604b995a9067398a7134
tkkrr <reioffitial@gmail.com>
2019-04-04 04:21:34 +0900

    ビルド設定を変更

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,48 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const worker = require.resolve('pdfjs-dist/build/pdf.worker.entry');
 
-module.exports = {
-    mode: "development",
-    entry: {
-        'main': path.resolve(__dirname, "./src/index.jsx"),
-        // 'pdf.worker': path.resolve(__dirname, "./node_modules/pdfjs-dist/build/pdf.worker.entry")
-        'pdf.worker': worker
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '',
-        filename: '[name].bundle.js',
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: '/node_modules/'
-            },
-            {
-                test: /\.(pdf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]'
+module.exports = (env, options) => {
+    
+    const buildPath = env ? env.path || "" : ""
+    
+    return {
+        mode: "production",
+        entry: {
+            'main': path.resolve(__dirname, buildPath, "./src/index.jsx"),
+            'pdf.worker': worker
+        },
+        output: {
+            path: path.resolve(__dirname, buildPath, './dist'),
+            publicPath: '',
+            filename: '[name].bundle.js',
+        },
+        module: {
+            rules: [
+                { 
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader',
+                    exclude: '/node_modules/'
+                },
+                {
+                    test: /\.(pdf)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]'
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, buildPath, './src/index.html')
+            })
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+    }
 }
